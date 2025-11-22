73f7fb5d8180a5977b57c70d38d9be80b00d7161
Naldo <lucirleylucas987@gmail.com>
2025-11-21 21:49:36 -0300

    Arquivos de next

---
+++ aulao-next-js/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,10 @@
+export default function Jsx(){
+    const a = 31
+    const b = 44
+    return (
+        <div>
+            <h1>Jsx é um conceito central</h1>
+            <p>A soma de {a} + {b} é: {a + b}</p>
+        </div>
+    )
+}
