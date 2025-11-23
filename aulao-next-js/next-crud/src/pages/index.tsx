b0a87f828c5e8a832773425b6c483c26afcb31a4
Naldo <lucirleylucas987@gmail.com>
2025-11-23 20:03:58 -0300

    Atualizei componentes de next

--- aulao-next-js/next-crud/src/pages/index.tsx
+++ aulao-next-js/next-crud/src/pages/index.tsx
@@ -1,15 +1,33 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
-  return (
-    <div className={`
-      flex justify-center items-center h-screen
-      bg-gradient-to-r from-blue-500 to-purple-600
-      text-white
-    `}>
-      <Layout titulo="Cadastro Simples">
-        Conteudo
-      </Layout>
-    </div>
-  );
+
+    const clientes = [
+      new Cliente('Ana', 23, '1'),
+      new Cliente('Pedro', 83, '2'),
+      new Cliente('Alice', 34, '3'),
+      new Cliente('Otavio', 23, '4'),
+    ]
+
+    function clienteSelecionado(cliente: Cliente) {
+      console.log(cliente.nome)
+    }
+
+    function clienteExcluido(cliente: Cliente) {
+      console.log(cliente.nome)
+    }
+
+    return (
+      <div className={`
+        flex justify-center items-center h-screen
+        bg-gradient-to-r from-blue-500 to-purple-600
+        text-white
+      `}>
+        <Layout titulo="Cadastro Simples">
+          <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
+        </Layout>
+      </div>
+    );
 }
